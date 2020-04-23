30a6eb415dc0c120a6afb719f613095a4b565f81
Eric DS <eric.d.s.info@gmail.com>
2020-04-23 15:28:49 +0100

    first version

--- include/bubble_sorting.hpp
+++ include/bubble_sorting.hpp
@@ -2,4 +2,19 @@
 #include "vector_t.hpp"
 
 template <class KEY>
-void bubble_sort(Vector_T<KEY> keys, int size){}
+void bubble_sort(Vector_T<KEY>& keys, int size)
+{
+    for (int i = 0; i < size -1; ++i)
+    {
+        for (int j = 0; j < size - 1; ++j)
+        {
+            if (keys[j] > keys[j + 1])
+            {
+                KEY aux = keys[j];
+                keys[j] = keys[j + 1];
+                keys[j + 1] = aux;
+            }
+
+        }
+    }
+}
