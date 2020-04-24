8ab96d510af08fa4f88faefbb067f87834d7ebc5
Eric DS <eric.d.s.info@gmail.com>
2020-04-24 12:33:27 +0100

    relative vector tested

--- tests/vector.test.hpp
+++ tests/vector.test.hpp
@@ -5,7 +5,7 @@
 
 SCENARIO("Testing the relative vector", "[vector]")
 {
-    GIVEN( "An empty vector" )
+    GIVEN( "A negative vector" )
     {
         Vector_T<int> my_vector(5,-2);
         WHEN( "started the elements" )
@@ -22,32 +22,36 @@ SCENARIO("Testing the relative vector", "[vector]")
                 std::cout << my_vector << "\n";
             }
         }
-        my_vector.~Vector_T();
     }
 
-    GIVEN( "A vector with defined  size and negative start" )
+    GIVEN( "A positive vector" )
     {
-        Vector_T<int> my_vector(5, -1);
-    
-        REQUIRE(my_vector.size()  == 5);
-        REQUIRE(my_vector.start() == -1);
-        REQUIRE(my_vector.end()   == 3);
-        
-        WHEN( "Added an element from the back " )
+        Vector_T<int> my_vector(5,2);
+        WHEN( "started the elements" )
         {
-            my_vector.push_back(1);
-            my_vector.push_back(3);
-            std::cout << my_vector << "\n";
-            THEN("Size is increased and end value is modified")
+            for (int i = my_vector.start(); i <= my_vector.end(); i++)
             {
-                CHECK(my_vector.size()  == 7);
-                CHECK(my_vector.start() == -1);
-                CHECK(my_vector.end()   == 5);
+                my_vector[i] = i;
+            }
+            THEN( "Size increased by 3 and start and end" )
+            {
+                CHECK(my_vector.size()  == 5);
+                CHECK(my_vector.start() == 2);
+                CHECK(my_vector.end()   ==  6);
+                std::cout << my_vector << "\n";
             }
         }
-
-        my_vector.~Vector_T();
+        AND_WHEN("pushed an element")
+        {
+            my_vector.push_back(9);
+            THEN( "Size increased by 1 and  and end is modified" )
+            {
+                CHECK(my_vector.size()  == 6);
+                CHECK(my_vector.start() == 2);
+                CHECK(my_vector.end()   ==  7);
+                std::cout << my_vector << "\n";
+            }
+        }
     }
-
 
 }
