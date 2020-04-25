b8d2ccdd405664ed7ba3c2da42339e2a17c2a6a2
Eric DS <eric.d.s.info@gmail.com>
2020-04-25 12:50:52 +0100

    solving conflicts

--- tests/includer.test.cpp
+++ tests/includer.test.cpp
@@ -2,8 +2,8 @@
 #include "../include/catch/catch.h"
 #include <iostream>
 
-#include "./bubble.test.hpp"
-#include "./insertion.test.hpp"
+#include "bubble.test.hpp"
+#include "insertion.test.hpp"
 #include "shell_sort.test.hpp"
 #include "quick_sort.test.hpp"
 //#include "vector.test.hpp"
